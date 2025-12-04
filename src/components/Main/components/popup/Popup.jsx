c3a34a470375bfae5d9b394375fc43a204e52967
Ricardo Castro <ricardo.caroj99@gmail.com>
2025-12-04 06:26:02 -0600

    fix: modificacion de estilos generales

--- src/components/Main/components/popup/Popup.jsx
+++ src/components/Main/components/popup/Popup.jsx
@@ -1,18 +1,35 @@
+import { useEffect } from 'react';
 import closeIcon from '../../../../images/close_icon.png';
 
-export default function Popup({
-  onClose,
-  title,
-  children,
-  variant, // "imagePopup" | "editPopup" | "addPopup" | "avatarPopup" | "confirmationPopup"
-}) {
+export default function Popup({ onClose, title, children, variant }) {
   const isImagePopup = variant === 'imagePopup';
 
   const containerClass = isImagePopup ? 'imagePopup__container' : 'popup__container';
-  const closeButtonClass = isImagePopup ? 'imagePopup__close-button' : `${variant}__close-button`;
+
+  // 👉 combinamos clase base + variante
+  const closeButtonClass = isImagePopup
+    ? 'imagePopup__close-button'
+    : variant
+      ? `popup__close-button ${variant}__close-button`
+      : 'popup__close-button';
+
   const closeIconClass = isImagePopup
     ? 'imagePopup__close-button-icon'
-    : `${variant}__close-button-icon`;
+    : variant
+      ? `popup__close-button-icon ${variant}__close-button-icon`
+      : 'popup__close-button-icon';
+
+  // CERRAR CON ESC
+  useEffect(() => {
+    function handleEscClose(e) {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    }
+
+    document.addEventListener('keydown', handleEscClose);
+    return () => document.removeEventListener('keydown', handleEscClose);
+  }, [onClose]);
 
   return (
     <div className={`popup ${variant || ''} popup_opened`} role="dialog" aria-modal="true">
